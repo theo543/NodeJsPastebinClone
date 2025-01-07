4ee605414b0f698929a575f5421e27585ea03262
Dragos <dragosionut2003@yahoo.ro>
2025-01-07 20:54:20 +0200

    Language model fix

--- models/language.js
+++ models/language.js
@@ -1,8 +1,7 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+import { Model } from 'sequelize';
+
+export default (sequelize, DataTypes) => {
   class Language extends Model {
     static associate(models) {
       // TBA
